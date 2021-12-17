63334e789b38aab6b81a6c1907055321d063ab6c
chiaraxs <chiaracardinale92@gmail.com>
2021-12-17 16:46:05 +0100

    added buttons

--- js/script.js
+++ js/script.js
@@ -6,8 +6,41 @@
 // con difficoltà 3 => tra 1 e 49
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
-// Consigli del giorno: 
-// Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-// Ad esempio: Di cosa ho bisogno per generare i numeri?
-// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-// Le validazioni e i controlli possiamo farli anche in un secondo momento.
+
+// 1. fisso i selettori tra js e html -> 3 buttons + container-box da replicare con ciclo for
+const easyLevelButton = document.getElementById("easy-button");
+const mediumLevelButton = document.getElementById("medium-button");
+const hardLevelButton = document.getElementById("hard-button");
+const boxes = document.querySelector('.container-box');
+
+// 2. creo funzione che per creare e appendere i boxes
+function createBoxGrid (container) {
+    const square = document.createElement('div');
+    square.className ='box';
+    container.append(square);
+
+    //stabilisco l'evento al click -> change bg in blue
+    square.addEventListener('click', function(){
+    this.classList.add('blue');   
+    })
+}
+
+// 3. creo ciclo for per replicare boxes all'evento click in base al lvl scelto
+easyLevelButton.addEventListener('click', function (){
+    for (let i = 1; i <= 100; i++) {
+        createBoxGrid(boxes); 
+    }
+});
+
+mediumLevelButton.addEventListener('click', function (){
+    for (let i = 1; i <= 81; i++) {
+        createBoxGrid(boxes); 
+    }
+});
+
+hardLevelButton.addEventListener('click', function (){
+    for (let i = 1; i <= 49; i++) {
+        createBoxGrid(boxes); 
+    }
+});
+
